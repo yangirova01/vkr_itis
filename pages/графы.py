8cba7cf7ab383d10227345132ef2f3ed9bc6b42d
Eveliva <yangirova01@bk.ru>
2023-06-13 12:15:32 +0100

    Update графы.py

--- pages/графы.py
+++ pages/графы.py
@@ -9,7 +9,7 @@ from streamlit_agraph import agraph, Node, Edge, Config
 st.sidebar.subheader("Настройки графа")
 width = st.sidebar.slider("Ширина", 500, 1200, 950)
 height = st.sidebar.slider("Высота", 500, 1200, 700)
-directed = st.sidebar.checkbox("Направленность граф", True)
+# directed = st.sidebar.checkbox("Направленность граф", True)
 physics = st.sidebar.checkbox("Физика", True)
 # hierarchical = st.sidebar.checkbox("Иерархический", False)
 load_file = st.sidebar.file_uploader("Загрузить файл", type=["csv"])
@@ -82,14 +82,13 @@ if not df.empty:
     #     hierarchical=hierarchical
     # )
 
-    config = Config(
-        width=width,
-        height=height,
-        directed=directed,
-        physics=physics,
-    )
+    # config = Config(
+    #     directed=directed,
+    #     physics=physics,
+    # )
 
     # Create the agraph using streamlit-agraph
-    return_value = agraph(nodes=nodes, edges=edges, config=config)
+    # return_value = agraph(nodes=nodes, edges=edges, config=config)
+    return_value = agraph(nodes=nodes, edges=edges, config={'directed': False, 'physics': True})
 else:
     st.warning('Данные не загружены. Загрузите файл CSV.')
